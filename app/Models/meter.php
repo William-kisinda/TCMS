00af8d711e2efbc30a8fe84e40456f171ace035d
Urio21 <hamphreyelisante@gmail.com>
2023-10-01 08:02:49 -0700

    debt implementation complete

--- app/Models/meter.php
+++ app/Models/meter.php
@@ -1,54 +1,98 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Meter extends Model
-
 {
     use HasFactory;
-    public $timestamps=false;
-    public $primarykey='id';
-    public $table = 'meters';
 
+    public $table = "meters";
+    public $timestamps = false;
+    public $primaryKey = 'id';
 
-    public function getMeterNumber(){
-      return  $this ->attributes['MeterNumber'];
-    }
-
-    public function setMeterNumber($MeterNumber){
-        $this ->attributes['MeterNumber']=$MeterNumber;
-    }
-
-    public function getMeterId(){
-      return  $this ->attributes['id'];
-    }
-    public function setMeterId($id){
-        $this ->attributes['MeterNumber']=$id;
-    }
-    public function getMeterStatus(){
-      return  $this ->attributes['status'];
-    }
-    public function setMeterStatus($status){
-        $this ->attributes['status']=$status;
-    }
-
-    public function getAttributes(){
-       return  $this ->attributes;
-    }
-    public function setAttributes($attributes){
-         $this ->attributes=$attributes;
-     }
-
-
-    public function debt()
+    /**
+     * @return mixed
+     */
+    public function getMeterId()
     {
-        return $this->hasOne(Debt::class, );
+        return $this->attributes['id'];
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setMeterId($id): void
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMeterNumber()
+    {
+        return $this->attributes['meter_number'];
+    }
+
+    /**
+     * @param mixed $meterNumber
+     */
+    public function setMeterNumber($meterNumber): void
+    {
+        $this->attributes['meter_number'] = $meterNumber;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getCustomerId()
+    {
+        return $this->attributes['customers_id'];
+    }
+
+    /**
+     * @param mixed $customerId
+     */
+    public function setCustomerId($customerId): void
+    {
+        $this->attributes['customers_id'] = $customerId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMeterStatus()
+    {
+        return $this->attributes['status'];
+    }
+
+    /**
+     * @param mixed $meterStatus
+     */
+    public function setMeterStatus($meterStatus): void
+    {
+        $this->attributes['status'] = $meterStatus;
     }
 
 
 
-}
+    /**
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
 
+    /**
+     * @param array $attributes
+     */
+    public function setAttributes($attributes)
+    {
+        $this->attributes = $attributes;
+    }
+}
